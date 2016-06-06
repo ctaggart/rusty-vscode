4746840e2fca29ac38e1a4284f300a58445a1025
Cameron Taggart <cameron@tachyus.com>
2016-06-06 11:31:21 -0700

    trailing slashes

--- root/install-xpra.sh
+++ root/install-xpra.sh
@@ -7,6 +7,6 @@ export DEBIAN_FRONTEND=noninteractive
 curl https://winswitch.org/gpg.asc | apt-key add - \
 && echo "deb http://winswitch.org/ jessie main" > /etc/apt/sources.list.d/winswitch.list \
 && apt-get update \
-&& apt-get install -y xpra
-&& curl -L -O https://github.com/ctaggart/xserver-xorg-video-dummy/releases/download/1_0.3.7-1.1/xserver-xorg-video-dummy_0.3.7-1.1_amd64.deb
+&& apt-get install -y xpra \
+&& curl -L -O https://github.com/ctaggart/xserver-xorg-video-dummy/releases/download/1_0.3.7-1.1/xserver-xorg-video-dummy_0.3.7-1.1_amd64.deb \
 && dpkg -i xserver-xorg-video-dummy_0.3.7-1.1_amd64.deb
