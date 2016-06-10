6ee9980750d5f6436cf03e788b70004be92ba160
Cameron Taggart <cameron.taggart@gmail.com>
2016-06-10 00:31:09 -0700

    actually installed via rustup

--- vscode/install-rust.sh
+++ vscode/install-rust.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 # https://www.rust-lang.org/downloads.html
+# https://www.rustup.rs/
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y build-essential file
-curl -sSf https://static.rust-lang.org/rustup.sh | sh
+#curl -sSf https://static.rust-lang.org/rustup.sh | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+echo 'export PATH=~/.cargo/bin:$PATH' >> ~/.bashrc
