8c04b650799ae88e90af0c1945a58f8cdff3cb4e
Cameron Taggart <cameron.taggart@gmail.com>
2016-06-14 22:25:58 -0700

    #20 fixed install of rustdt

--- root/install-rustdt.sh
+++ root/install-rustdt.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -e
+export JAVA_HOME=/opt/jdk
+export PATH=$JAVA_HOME/bin:$PATH
 /opt/eclipse/eclipse -nosplash -consolelog -clean \
   -application org.eclipse.equinox.p2.director \
   -installIU com.github.rustdt.feature.group \
